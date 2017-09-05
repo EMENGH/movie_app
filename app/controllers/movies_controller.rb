1ba915a72e5a5cd425d91d4f781891f17403b3a5
EMENGH <eamendez62@gmail.com>
2017-09-04 21:39:36 -0400

    added routes and views

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,42 @@
+class MoviesController < ApplicationController
+  def index
+      @movies = Movie.all 
+  end
+
+  def new
+      @movie = Movie.new
+  end
+
+  def create
+      @movie = Movie.create!(movie_params)
+
+      redirect_to root_path
+  end
+
+  def edit
+      @movie = Movie.find(params[:id])
+  end
+
+  def update
+      @movie = Movie.find(params[:id])
+      @movie.update(movie_params)
+
+      redirect_to movie_path(@movie.id)
+  end
+
+  def destroy
+      @movie = Movie.find(params[:id])
+      @movie.destroy
+
+      redirect_to movies_path
+  end
+
+  def show
+      @movie = Movie.find(params[:id])
+  end
+
+  private
+  def movie_params
+      params.require(:movie).permit(:title, :genre, :year, :plot, :picture)
+end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'fights#index'
+  # get 'fights/index'
+
+  get '/movies', to: 'movies#index'
+  put '/movies/:id', to: 'movies#update'
+  patch '/movies/:id', to: 'movies#update', as: 'update_movie'
+
+  # put '/pokemon/:id', to: 'pokemon#update'
+  # patch '/pokemon/:id', to: 'pokemon#update', as: 'update_pokemon'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
